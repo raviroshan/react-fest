3c1a5034fd33d1653415be722ec4c37c6112a4d8
praveen divekar <pdivekar@deloitte.com>
2018-08-07 20:51:05 +0530

    Header component

--- src/react-app/components/AppMain/AppMain.jsx
+++ src/react-app/components/AppMain/AppMain.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Header/Header';
 import './AppMain.scss';
 
 const AppMain = () => {
   const welcomeMessage = 'Welcome to React fest';
   return (
     <div className="container">
+      <Header />
       <div className="row">
         <h2>{welcomeMessage}</h2>
       </div>
