e6d83fe07451ac115bea3344e3b6451b766c3896
praveen divekar <pdivekar@deloitte.com>
2018-08-08 21:08:43 +0530

    Final commit

--- src/react-app/components/AppMain/AppMain.jsx
+++ src/react-app/components/AppMain/AppMain.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import './AppMain.scss';
 
 const AppMain = () => {
@@ -7,6 +8,7 @@ const AppMain = () => {
   return (
     <div className="container">
       <Header />
+      <h1> Tile Component </h1>
       <Footer />
     </div>
   );
